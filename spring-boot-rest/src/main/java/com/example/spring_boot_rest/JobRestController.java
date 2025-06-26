b2fbb64a72fd2729ce8654ec4792c749a77fa55c
Uday Reddy <udayreddy.vk@gmail.com>
2025-06-26 09:20:50 +0530

    Initial commit

---
+++ spring-boot-rest/src/main/java/com/example/spring_boot_rest/JobRestController.java
@@ -0,0 +1,52 @@
+package com.example.spring_boot_rest;
+
+import com.example.spring_boot_rest.model.JobPost;
+import com.example.spring_boot_rest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+
+import java.util.List;
+
+@Controller()
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping("jobPosts")
+    @ResponseBody
+    public List<JobPost> getAllJobs(){
+        return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    public JobPost getJob(@PathVariable("postId") int postId){
+        return service.getJob(postId);
+    }
+
+    @PostMapping("jobPost")
+    public void addJob(@RequestBody JobPost jobPost){
+        service.addJob(jobPost);
+    }
+
+//    @GetMapping("jobPosts/keyword/{keyword}")
+//    public List<JobPost> searchByKeyword(@PathVariable("keyword") String keyword){
+//        return service.search(keyword);
+//    }
+
+    @GetMapping("/load")
+    @ResponseBody
+    public String loadData(){
+        System.out.println("Load endpoint hit!");
+        service.load();
+
+        return "success";
+    }
+}
